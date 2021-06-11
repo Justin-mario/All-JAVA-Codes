59e44270d74714de99d25ea65d192642ef0fa85b
Chigozie Nwachukwu <ppaddy766@gmail.com>
2021-06-11 01:18:04 +0100

    CLASS EXERCISES

---
+++ src/com/ClassWork/SevenSegmentDisplay.java
@@ -0,0 +1,108 @@
+package com.ClassWork;
+
+public class SevenSegmentDisplay {
+
+    private int[][] sevenSegmentArray = new int[5][4];
+
+    public void display() {
+        for (int[] rows : sevenSegmentArray) {
+            for (int column : rows) {
+                if (column == 1)
+                    System.out.print ( '#' + "  " );
+                if (column == 0)
+                    System.out.print ( "   " );
+            }
+            System.out.println ();
+            System.out.println ();
+        }
+    }
+
+    public void writeOnA() {
+        for (int column = 0; column < 4; column++)
+            sevenSegmentArray[0][column] = 1;
+    }
+
+    public void writeOnB() {
+        for (int row = 0; row < 3; row++)
+            sevenSegmentArray[row][3] = 1;
+    }
+
+    public void writeOnC() {
+        for (int row = 2; row < 5; row++)
+            sevenSegmentArray[row][3] = 1;
+    }
+
+    public void writeOnD() {
+        for (int column = 3; column >= 0; column--)
+            sevenSegmentArray[4][column] = 1;
+    }
+
+    public void writeOnE() {
+        for (int row = 4; row >= 2; row--)
+            sevenSegmentArray[row][0] = 1;
+    }
+
+    public void writeOnF() {
+        for (int row = 0; row < 3; row++)
+            sevenSegmentArray[row][0] = 1;
+    }
+
+    public void writeOnG() {
+        for (int column = 0; column < 3; column++)
+            sevenSegmentArray[2][column] = 1;
+    }
+
+    public void setUp(String userInput) {
+        for (int i = 0; i < userInput.length (); i++) {
+            while(userInput.length () < 8){
+                userInput = "0" + userInput ;
+            }
+            validateInput ( userInput );
+            if (userInput.charAt ( 7 ) == '0')
+                userInput = " ";
+
+            if (userInput.charAt ( i ) == '1') {
+                switch (i) {
+                    case 0 -> {
+                        writeOnA ();
+                    }
+                    case 1 -> {
+                        writeOnB ();
+                    }
+                    case 2 -> {
+                        writeOnC ();
+                    }
+                    case 3 -> {
+                        writeOnD ();
+                    }
+                    case 4 -> {
+                        writeOnE ();
+                    }
+                    case 5 -> {
+                        writeOnF ();
+                    }
+                    case 6 -> {
+                        writeOnG ();
+                    }
+                        }
+                    }
+                }
+            }
+
+
+
+
+
+    public void validateInput(String userInput) {
+        for (int i = 0; i < userInput.length (); i++) {
+            if (userInput.length () > 8){
+                throw new IndexOutOfBoundsException();
+            }
+            if ( userInput.charAt ( i ) != '1' && userInput.charAt ( i ) != '0') {
+                throw new IllegalArgumentException ();
+            }
+        }
+    }
+}
+
+
---
+++ src/com/ClassWork/SevenSegmentDisplayApp.java
@@ -0,0 +1,21 @@
+package com.ClassWork;
+
+public class SevenSegmentDisplayApp {
+    public static void main(String[] args) {
+
+        SevenSegmentDisplay segmentDisplay = new SevenSegmentDisplay ();
+        String userInput = "1111";
+
+        try {
+            segmentDisplay.setUp ( userInput );
+        }
+        catch (IndexOutOfBoundsException e){
+            System.out.println ("Length Of Input Must Not Be More Than Eight");
+        }
+        catch (IllegalArgumentException e){
+            System.out.println ("Value Of Input Must Be 0 or 1");
+        }
+
+        segmentDisplay.display (  );
+    }
+}
